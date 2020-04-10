de078e3709f45280bbdced3c526dfd121564cf16
Liliya Gavrishchishena <liliya.gavrishchishena@gmail.com>
2020-04-10 20:54:13 +0300

    functionality was added

--- public/app.js
+++ public/app.js
@@ -8,6 +8,16 @@ new Vue({
       todos: [],
     };
   },
+  created() {
+    fetch('/api/todo', {
+        method: 'get',
+      })
+      .then((res) => res.json())
+      .then(todos => {
+        this.todos = todos
+      })
+      .catch((e) => console.log(e))
+  },
   methods: {
     addTodo() {
       const title = this.todoTitle.trim();
@@ -15,36 +25,68 @@ new Vue({
         return;
       }
       fetch('/api/todo', {
-        method: 'post',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          title,
-        }),
-      })
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            title,
+          }),
+        })
         .then((res) => res.json())
-        .then(({ todo }) => {
-          console.log(todo);
+        .then(({
+          todo
+        }) => {
           this.todos.push(todo);
           this.todoTitle = '';
         })
         .catch((e) => console.log(e));
     },
     removeTodo(id) {
-      this.todos = this.todos.filter((t) => t.id !== id);
+      fetch('/api/todo/' + id, {
+          method: 'delete'
+        })
+        .then(() => {
+          this.todos = this.todos.filter((t) => t.id !== id);
+        })
+        .catch((e) => console.log(e));
     },
+    completeTodo(id) {
+      fetch('/api/todo/' + id, {
+          method: 'put',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            done: true
+          }),
+        })
+        .then((res) => res.json())
+        .then(({
+          todo
+        }) => {
+          const idx = this.todos.findIndex(t => t.id === todo.id)
+          this.todos[idx].updatedAt = todo.updatedAt
+        })
+        .catch((e) => console.log(e));
+    }
   },
   filters: {
     capitalize(value) {
       return value.toString().charAt(0).toUpperCase() + value.slice(1);
     },
-    date(value) {
-      return new Intl.DateTimeFormat('ru-RU', {
+    date(value, withTime) {
+      const options = {
         year: 'numeric',
         month: 'long',
         day: '2-digit',
-      }).format(new Date(value));
+      };
+      if (withTime) {
+        options.hour = '2-digit'
+        options.minute = '2-digit'
+        options.second = '2-digit'
+      }
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date(value));
     },
   },
 });
--- routes/todo.js
+++ routes/todo.js
@@ -5,11 +5,12 @@ const Todo = require('../models/todo')
 const router = Router();
 
 // Get list of all todos
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   try {
-
+    const todos = await Todo.findAll();
+    res.status(200).json(todos);
   } catch (e) {
-    console.log(e)
+    console.log(e);
     res.status(500).json({
       message: "Server error"
     })
@@ -35,8 +36,14 @@ router.post('/', async (req, res) => {
 })
 
 // Modify todo item
-router.post('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   try {
+    const todo = await Todo.findByPk(+req.params.id)
+    todo.done = req.body.done
+    await todo.save()
+    res.status(200).json({
+      todo
+    })
 
   } catch (e) {
     console.log(e)
@@ -47,9 +54,18 @@ router.post('/:id', (req, res) => {
 })
 
 // Delete todo item
-router.post('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   try {
+    const todos = await Todo.findAll({
+      where: {
+        id: +req.params.id
+      }
+    })
+    const todo = todos[0]
+    await todo.destroy()
+    res.status(204).json({
 
+    })
   } catch (e) {
     console.log(e)
     res.status(500).json({
